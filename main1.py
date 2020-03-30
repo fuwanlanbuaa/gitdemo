b442ef6e9bf6ccd57d2410046643b744bf061f60
fuwanlan <919129166@qq.com>
2020-03-30 22:40:25 +0800

    the beginning

---
+++ main1.py
@@ -0,0 +1,10 @@
+from tkinter import *
+import tkinter.messagebox as mb
+from pylab import mpl
+import matplotlib.pyplot as plt
+import webbrowser
+import re
+mpl.rcParams['font.sans-serif'] = ['SimHei'] 
+
+stoplist1=open('stoplist.txt','r')
+stoplist=stoplist1.read()
